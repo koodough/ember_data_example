4ec5055ba3bcb288d1ef4cbda049e509f3aafa98
Koodough <koodough@koodough.com>
2013-01-06 15:13:31 -0600

    :key =>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-EmberDataExample::Application.config.session_store :cookie_store, key: '_ember_data_example_session'
+EmberDataExample::Application.config.session_store :cookie_store, :key => '_ember_data_example_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
